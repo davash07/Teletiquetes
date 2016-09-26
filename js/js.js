1c17e8c105757d8dad6c4fb2ebb352ddae526874
David Vasquez <david.vasquez@sundevs.com>
2016-09-26 08:53:13 -0500

    add teletiquetes-web

---
+++ js/js.js
@@ -0,0 +1,17 @@
+  $(".button-collapse").sideNav();
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+      // Pause slider
+      $('.slider').slider('pause');
+      // Start slider
+      $('.slider').slider('start');
+      // Next slide
+      $('.slider').slider('next');
+      // Previous slide
+      $('.slider').slider('prev');
+ 
+ });
+ $(document).ready(function(){
+      $('.slider2').slider({indicators:false, height: 320});
+    });
+        
